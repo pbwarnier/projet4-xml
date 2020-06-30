d459b2740ddf536119d194391db933299a7cca4e
mak <lievrard.maxime@icloud.com>
2020-06-30 11:40:31 +0200

    formulaire

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('form');
+
+button = document.createElement('button');
+button.textContent = 'Envoyer';
+button.setAttribute('type', 'submit');
+
+document.querySelector('form').insertAdjacentElement('beforeend', button);
